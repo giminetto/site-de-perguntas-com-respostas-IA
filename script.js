b30c2587429e6aff2bf17e0a380dd4e9dbc2e34d
giminetto <130772561+giminetto@users.noreply.github.com>
2024-08-14 08:34:16 -0300

    erro

--- script.js
+++ script.js
@@ -6,65 +6,63 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-<<<<<<< HEAD
-        enunciado: "De acordo com a Band News FM Curitiba, mais de 30% dos jovens ainda não sabem qual profissão seguir. Você tem conhecimento de qual área deseja seguir na faculdade?",
-=======
-        enunciado: "a maioria das pessoas tem amizades superficiais e poucas investem seu tempo nas redes em atividades realmente importantes, e outras apenas perdem tempo nas redes, não adicionando conhecimentos nem aproveitando tudo oque a tecnologia pode nos proporcionar.",
->>>>>>> 7673402a03cdfb4c3d93ec03c2b4ca3ee97ec041
+
+        enunciado: "De acordo com a Band News FM Curitiba, mais de 30% dos jovens ainda não sabem qual profissão seguir. Você tem conhecimento de qual área deseja seguir na faculdade?",        enunciado: "a maioria das pessoas tem amizades superficiais e poucas investem seu tempo nas redes em atividades realmente importantes, e outras apenas perdem tempo nas redes, não adicionando conhecimentos nem aproveitando tudo oque a tecnologia pode nos proporcionar.",
+
         alternativas: [
             {
-<<<<<<< HEAD
+
                 texto: "sim",
-                afirmacao: "Otimo! É preciso ter conhecimento de qual área seguir, para garantir um futuro mais próspero. "
-=======
+                afirmacao: "Otimo! É preciso ter conhecimento de qual área seguir, para garantir um futuro mais próspero. ",
+
                 texto: "você investe ou perde tempo nas redes sociais?",
                 afirmacao: "eu invisto procurando e pesquisando imformações que tenho curiosidade, aprendendo novas coisas. "
->>>>>>> 7673402a03cdfb4c3d93ec03c2b4ca3ee97ec041
+
             },
             {
-<<<<<<< HEAD
+
                 texto: "não",
-                afirmacao: "É preciso ter conhecimento das áreas, para ver qual mais se encaixa com seu perfil, para que, assim, garanta um futuro mais próspero."
-=======
+                afirmacao: "É preciso ter conhecimento das áreas, para ver qual mais se encaixa com seu perfil, para que, assim, garanta um futuro mais próspero.",
+
                 texto: "ou você pensa que as redes foram criadas apenas para distrações, para passar o tempo e nada mais.",
                 afirmacao: "penso que as redes foram criadas apenas para distrações, e passar tempo"
->>>>>>> 7673402a03cdfb4c3d93ec03c2b4ca3ee97ec041
+
             }
         ]
     },
     {
-<<<<<<< HEAD
+
         enunciado: "Dentro dos cursos superiores, qual área mais se encaixa com você?",
-=======
+
         enunciado: "uma pesquisa internacional feita por um site de tecnologia mostrou que os brasileiros são praticamente recordistas em tempo conectado a telas de celulasres ou outros eletrônicos. apopulação fica em média nove horas e meia por dia.",
->>>>>>> 7673402a03cdfb4c3d93ec03c2b4ca3ee97ec041
+
         alternativas: [
             {
-<<<<<<< HEAD
+
                 texto: "Área de humanas",
-                afirmacao: "Existem muitos ótimos cursos na área de humanas, sendo eles: Direito, pedagogia, psicologia, jornalismo, entre outros."
-=======
+                afirmacao: "Existem muitos ótimos cursos na área de humanas, sendo eles: Direito, pedagogia, psicologia, jornalismo, entre outros.",
+
                 texto: "você fica mais de 9hrs investindo seu tempo nas redes? E você acha que está certo?",
                 afirmacao: "estou certo se estou aprendendo e investindo o meu tempo nisso.  "
->>>>>>> 7673402a03cdfb4c3d93ec03c2b4ca3ee97ec041
+
             },
             {
-<<<<<<< HEAD
+
                 texto: "Área de exatas",
-                afirmacao: "Existem muitos ótimos cursos na área de exatas, sendo eles: Engenharia, arquitetura, gestão financeira, matematica, entre outros."
-=======
+                afirmacao: "Existem muitos ótimos cursos na área de exatas, sendo eles: Engenharia, arquitetura, gestão financeira, matematica, entre outros.",
+
                 texto: "você fica mais de 9hrs nas redes por distrações? E você acha que está certo?",
                 afirmacao: "acho que estou certo pois gosto de passar meu tempo nas tecnologias"
->>>>>>> 7673402a03cdfb4c3d93ec03c2b4ca3ee97ec041
+
             }
         ]
     },
     {
-<<<<<<< HEAD
+
         enunciado: "As faculdades EAD estão ganhando cada vez mais vizibilidade no mundo atual, você teria interesse em se matricular em uma faculdade online?",
-=======
+
         enunciado: "",
->>>>>>> 7673402a03cdfb4c3d93ec03c2b4ca3ee97ec041
+
         alternativas: [
             {
                 texto: "Sim",
@@ -76,32 +74,7 @@ const perguntas = [
             }
         ]
     },
-    {
-        enunciado: "Enunciado 4",
-        alternativas: [
-            {
-                texto: "texto botão 1",
-                afirmacao: "Afirmação do texto botão 1"
-            },
-            {
-                texto: "texto botão 2",
-                afirmacao: "Afirmação do texto botão 2"
-            }
-        ]
-    },
-    {
-        enunciado: "Enunciado 5",
-        alternativas: [
-            {
-                texto: "texto botão 1",
-                afirmacao: "Afirmação do texto botão 1"
-            },
-            {
-                texto: "texto botão 2",
-                afirmacao: "Afirmação do texto botão 2"
-            }
-        ]
-    },
+   
    
 ];
 
